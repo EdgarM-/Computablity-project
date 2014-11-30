9d8970b136bb6b7957aae57b8187baad28ce303e
EdgarM- <ed.amezqui.95@gmail.com>
2014-11-30 03:51:01 -0500

    Reorganizes the class, added string test for fsa, and finishes the archive management(All works :D)

--- fsa.cpp
+++ fsa.cpp
@@ -1,54 +1,84 @@
 #include <algorithm>
 #include <utility> 
+#include <sstream>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-
-class Node
-{
-public:
-	int initialState;
-	int finalState;
-	std::vector< std::pair< int,std::string >  > conections;
-	Node(int isInitial, int isFinal)
-	{
-		initialState = isInitial;
-		finalState = isFinal;
-	}
-	~Node()
-	{}
-
-	/* data */
-	void AddConnection(int state, std::string transition)
-	{
-		std::pair< int,std::string > newcon;
-		newcon = std::make_pair(state,transition);
-		conections.push_back(newcon);
-	}
-};
-
+#include <cstring>
 
 class Fsa
 {
 public:
-	std::vector< std::vector< std::string > > matrizAdyacencia;
-	std::vector<Node> states;
-	Fsa(int vertices)
+	std::vector< std::vector< std::string > > matrizAdyacencia; //Matriz para representar el Grafo
+	std::vector<int> initialStates; //Lista de estados iniciales
+	std::vector<int> finalStates; //Lista de estado finales
+	std::string name; //Nombre del archivo, esta vacio hasta que se guarda o se carga
+	Fsa()
 	{
-		matrizAdyacencia.resize(vertices);
-		//states.resize(states);
-		for (int i = 0; i < vertices; ++i)
+
+	}
+	/*
+		Crea un Fsa con n nodos 
+	*/
+	Fsa(int nodos)
+	{
+		matrizAdyacencia.resize(nodos);
+		for (int i = 0; i < nodos; ++i)
 		{
-			matrizAdyacencia[i].resize(vertices);
+			matrizAdyacencia[i].resize(nodos);
 		}
+	}
+	/*
+		Crea un Fsa con n nodos con un estado inicial y un estado final
+	*/
+	Fsa(int nodos, int inicial, int final)
+	{
+		matrizAdyacencia.resize(nodos);
+		for (int i = 0; i < nodos; ++i)
+		{
+			matrizAdyacencia[i].resize(nodos);
+		}
+		initialStates.push_back(inicial);
+		finalStates.push_back(final);
+	}
+
+	Fsa(int nodos, std::vector<int> iniciales, std::vector<int> finales)
+	{
+		matrizAdyacencia.resize(nodos);
+		for (int i = 0; i < nodos; ++i)
+		{
+			matrizAdyacencia[i].resize(nodos);
+		}
+		initialStates = iniciales;
+		finalStates = finales;
 	}
 	~Fsa()
 	{
 
 	}
+	/*
+		Añade un nodo inicial
+	*/
+	void AddIni(int nodo)
+	{
+		initialStates.push_back(nodo);
+	}
+	/*
+		Añade un nodo final
+	*/
+	void AddFin(int nodo)
+	{
+		finalStates.push_back(nodo);
+	}
+	/*
+		Añade una transicion del nodo n1 al nodo n2 con el caracter transition
+	*/
+	void AddTran(int n1, int n2, std::string transition)
+	{
+		matrizAdyacencia[n1-1][n2-1] = transition;
+	}
 
-	/* data */
 
 	/*
 	Escribe un archivo con la descripcion del fsa y el nombre dado
@@ -56,20 +86,22 @@ public:
 	void GuardarFsa(std::string nombre)
 	{
 		std::ofstream saveFile;
+		name = nombre;
 		nombre += ".fsa";
 		saveFile.open (nombre.c_str());
 		int size = matrizAdyacencia.size();
-  		saveFile << size << "\n";
-  		for (int i = 0; i < states.size(); ++i)
+		int sizeIS = initialStates.size();
+		int sizeFS = finalStates.size();
+  		saveFile << size << "\n"<< sizeIS << "\n"<< sizeFS << "\n";
+  		for (int i = 0; i < sizeIS; ++i)
   		{
-  			if (states[i].initialState == 1)
-  			{
-  				saveFile << states[i].initialState << "\n";
-  			}
-  			if (states[i].finalState == 1)
-  			{
-  				saveFile << states[i].finalState << "\n";
-  			}
+  			saveFile << initialStates[i] << "\n";
+  			
+  		}
+  		for (int i = 0; i < sizeFS; ++i)
+  		{
+  			saveFile << finalStates[i] << "\n";
+  			
   		}
   		for (int i = 0; i < size; ++i)
   		{
@@ -92,23 +124,88 @@ public:
 	{
 		std::ifstream loadFile;	
 		loadFile.open(path.c_str());
-		int size,tmp1, tmp2,initial, final;
+		int size, sizeIS, sizeFS,tmp1, tmp2;
 		std::string transition;
 		if (loadFile.is_open())
   		{
-  			loadFile >> size >> initial >> final;
+  			loadFile >> size >> sizeIS >> sizeFS;
   			matrizAdyacencia.resize(size);
 			for (int i = 0; i < size; ++i)
 			{
 				matrizAdyacencia[i].resize(size);
 			}
-  			std::cout << size << " " << initial << " "<< final << '\n';
+			for (int i = 0; i < sizeIS; ++i)
+  			{	
+  				loadFile >> tmp1;
+  				initialStates.push_back(tmp1);
+  			
+  			}
+  			for (int i = 0; i < sizeFS; ++i)
+  			{	
+  				loadFile >> tmp1;
+  				finalStates.push_back(tmp1);
+  			
+  			}
   		  	while ( loadFile >> tmp1 )
   		  	{
   		  		loadFile >> tmp2 >> transition;	
-	  		    std::cout << tmp1 << " " << tmp2 << " "<< transition<< '\n';
+	  		    matrizAdyacencia[tmp1][tmp2] = transition;
   			}
   			loadFile.close();
+  		}
+	}
+	/*
+		Revisa si la cadena es aceptada por el fsa. Devuelve 1 en caso de que sea aceptada, 0 en los demas casos
+	*/
+	int ComprobarCadena(std::string entrada)
+	{
+		int estadoActual = initialStates[0]-1, size = matrizAdyacencia.size(), tmp = 0;	
+
+		for(int i = 0; i < entrada.size(); ++i)
+		{	
+			for (int j = 0; j < size; ++j)
+			{				
+				if (matrizAdyacencia[estadoActual][j][0] == entrada[i] )
+				{
+					estadoActual = j;
+					break;
+					
+				}
+			}
+		}	
+		for (int i = 0; i < finalStates.size(); ++i)
+		{
+			if (finalStates[i]-1 == estadoActual)
+			{
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+
+	/*
+		Muestra la matriz de adyacencia y los estado iniciales y finales
+	*/
+	void MostrarFsa()
+	{
+		std::cout << "FSA\n Estados Iniciales: \n";
+		for(auto& a : initialStates)
+			std::cout << a << "\n";
+		std::cout << "Estados Finales: \n";
+		for(auto& a : finalStates)
+			std::cout << a << "\n";
+		std::cout << "Transiciones\n";
+		int size = matrizAdyacencia.size();
+		for (int i = 0; i < size; ++i)
+  		{
+  			for (int j = 0; j < size; ++j)
+  			{
+  				if (matrizAdyacencia[i][j] != "")
+  				{
+  					std::cout << "Se va de "<<i+1 << " a " << j+1 << " con " << matrizAdyacencia[i][j]<< "\n";
+  				}
+  			}
   		}
 	}
 };
@@ -116,6 +213,24 @@ public:
 int main(int argc, char const *argv[])
 {
 	Fsa prueba(5);
+	prueba.AddIni(1);
+	prueba.AddFin(5);
+	prueba.AddTran(1,2,"0");
+	prueba.AddTran(2,3,"1");
+	prueba.AddTran(4,2,"0");
+	prueba.AddTran(3,5,"1");
+	prueba.MostrarFsa();
+	std::string respuesta;
+	std::cout << "Aceptacion de cadenas\n";
+	std::cout << "Caso 1\n";
+	respuesta = (prueba.ComprobarCadena("011") == 1) ? "Yes\n" : "NO\n";
+	std::cout<< respuesta;
+	std::cout << "Caso 2\n";
+	respuesta = (prueba.ComprobarCadena("010") == 1) ? "Yes\n" : "NO\n";
+	std::cout<< respuesta;
+	std::cout << "Fin de Aceptacion de cadenas\n";
+	prueba.GuardarFsa("pruebasave");
  	prueba.CargarFsa("pruebasave.fsa"); 	
+	prueba.MostrarFsa();
 	return 0;
 }
